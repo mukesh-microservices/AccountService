fe455f5d9bdea289cf1926c38ea77a9aab370f3e
Mukesh <mail2mkumar@gmail.com>
2019-02-07 14:34:24 -0600

    Added sleuth

--- src/main/java/com/demo/microservices/AccountService/controller/AccountDetailsController.java
+++ src/main/java/com/demo/microservices/AccountService/controller/AccountDetailsController.java
@@ -1,5 +1,7 @@
 package com.demo.microservices.AccountService.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,31 +9,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.demo.microservices.AccountService.serviceProxy.LoyalyServiceProxyFeign; 
+import com.demo.microservices.AccountService.serviceProxy.LoyalyServiceProxyFeign;
 
 @RestController
 public class AccountDetailsController {
-	
+
+	static Logger logger = LoggerFactory.getLogger(AccountDetailsController.class) ;
+
 	@Autowired
-	private LoyalyServiceProxyFeign loyalyServiceProxyFeign  ;
-	
-	RestTemplate restTemplate = new RestTemplate();
-	
+	private LoyalyServiceProxyFeign loyalyServiceProxyFeign;
+
+	@Autowired
+	RestTemplate restTemplate;
+
 	private String loyaltyURL = "http://192.168.0.7:8083/points/";
 
 	@GetMapping("/accountDetails/{userId}")
 	public String getAccountDetails(@PathVariable String userId) {
 
-		ResponseEntity<String> response = restTemplate.getForEntity(loyaltyURL + userId, String.class);
+		logger.info("Before calling Loyalty Service.");
 
-		return "This is account details wilt loyalyu points - " + response.getBody();
+		ResponseEntity<String> response = restTemplate.getForEntity(loyaltyURL + userId, String.class);
+		logger.info("After calling Loyalty Service.");
+
+		return "REST - This is account details wilt loyalyu points - " + response.getBody();
 	}
-	
+
 	@GetMapping("/accountDetails-feign/{userId}")
 	public String getAccountDetailsFeign(@PathVariable String userId) {
-		
+
 		String points = loyalyServiceProxyFeign.getPoints(userId);
 		return "This is account details wilt loyalyu points - " + points;
 	}
-	
+
 }
